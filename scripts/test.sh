724931b49846064a62a0a640b6de846651683c7c
Lup Yuen Lee <luppy@appkaki.com>
2025-12-27 22:26:51 +0800

    Test SG2000 via SSH
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 ## Automated Testing of Apache NuttX RTOS on Sophgo SG2000 SoC / Milk-V Duo S SBC
 
+echo Now running https://github.com/lupyuen2/autotest-nuttx-sg2000/blob/main/scripts/test.sh
+
 set -e  ##  Exit when any command fails
 set -x  ##  Echo commands
+
+## Server that controls Oz64 SG2000. And the TFTP Server.
+export OZ64_SERVER=tftpserver
+export TFTP_SERVER=tftpserver
 
 ##  Default Build Prefix is "nuttx-sg2000"
 if [ "$BUILD_PREFIX" == '' ]; then
@@ -12,11 +18,6 @@ fi
 ##  Default Build Date is today (YYYY-MM-DD)
 if [ "$BUILD_DATE" == '' ]; then
     export BUILD_DATE=$(date +'%Y-%m-%d')
-fi
-
-##  Default USB Device is /dev/tty.usbserial-0001
-if [ "$USB_DEVICE" == '' ]; then
-    export USB_DEVICE=/dev/tty.usbserial-0001
 fi
 
 set +x  ##  Disable echo
@@ -42,138 +43,32 @@ fi
 ##  Write the Release Tag for populating the Release Log later
 echo "$BUILD_PREFIX-$BUILD_DATE" >/tmp/release.tag
 
-## Copy NuttX Image to TFTP Server
-echo "----- Copy NuttX Image to TFTP Server"
+echo ----- Download the Device Tree
 set -x  ##  Enable echo
-scp /tmp/Image tftpserver:/tftpboot/Image-sg2000
-ssh tftpserver ls -l /tftpboot/Image-sg2000
-rm /tmp/Image
+pushd /tmp
+rm -f cv181x_milkv_duos_sd.dtb
+wget https://github.com/lupyuen2/wip-nuttx/releases/download/sg2000-1/cv181x_milkv_duos_sd.dtb
+popd
 set +x  ##  Disable echo
-date
 
-echo "----- Close the screen session"
+echo ----- Copy NuttX Binary Image and Device Tree to TFTP Server
 set -x  ##  Enable echo
-$SCRIPT_DIR/close.exp || true
+pushd /tmp
+scp cv181x_milkv_duos_sd.dtb $TFTP_SERVER:/tftpboot/cv181x_milkv_duos_sd.dtb
+scp Image $TFTP_SERVER:/tftpboot/Image-sg2000
+ssh $TFTP_SERVER ls -l /tftpboot/Image-sg2000
+rm Image cv181x_milkv_duos_sd.dtb
+popd
 set +x  ##  Disable echo
-date
 
-## Without a Smart Power Plug:
-## echo Power off the SBC, press Enter, then power on...
-## read
-
-## Get the Home Assistant Token, copied from http://localhost:8123/profile/security
-## token=xxxx
-set +x  ##  Disable echo
-. $HOME/home-assistant-token.sh
+echo ----- Run the NuttX Test
 set -x  ##  Enable echo
-
+pushd /tmp
+rm -f oz64.exp oz64-power.sh
+wget https://raw.githubusercontent.com/lupyuen/nuttx-release/refs/heads/main/oz64.exp
+wget https://raw.githubusercontent.com/lupyuen/nuttx-release/refs/heads/main/oz64-power.sh
+chmod +x oz64.exp oz64-power.sh
+expect ./oz64.exp
+rm oz64.exp oz64-power.sh
+popd
 set +x  ##  Disable echo
-date
-echo "----- Power Off the SBC"
-curl \
-    -X POST \
-    -H "Authorization: Bearer $token" \
-    -H "Content-Type: application/json" \
-    -d '{"entity_id": "automation.sg2000_power_off"}' \
-    http://localhost:8123/api/services/automation/trigger
-set -x  ##  Enable echo
-
-set +x  ##  Disable echo
-date
-echo "----- Power On the SBC"
-curl \
-    -X POST \
-    -H "Authorization: Bearer $token" \
-    -H "Content-Type: application/json" \
-    -d '{"entity_id": "automation.sg2000_power_on"}' \
-    http://localhost:8123/api/services/automation/trigger
-set -x  ##  Enable echo
-
-##  Run the Automated Test
-set +x  ##  Disable echo
-date
-echo "----- Run the Automated Test"
-set -x  ##  Enable echo
-$SCRIPT_DIR/nuttx.exp
-
-## Without a Smart Power Plug:
-## echo Power off the SBC
-
-set +x  ##  Disable echo
-date
-echo "----- Power Off the SBC"
-curl \
-    -X POST \
-    -H "Authorization: Bearer $token" \
-    -H "Content-Type: application/json" \
-    -d '{"entity_id": "automation.sg2000_power_off"}' \
-    http://localhost:8123/api/services/automation/trigger
-date
-set -x  ##  Enable echo
-
-##  TODO: Check whether NuttX has crashed
-# set +e  ##  Don't exit when any command fails
-# match=$(grep "registerdump" /tmp/test.log)
-# set -e  ##  Exit when any command fails
-
-# if [ "$match" == "" ]; then
-#     ##  If BL602 has not crashed, send the test command to BL602
-# else
-#     ##  If BL602 has crashed, do the Crash Analysis
-#     echo; echo "===== Boot FAILED. Below is the Crash Analysis"; echo
-
-#     ##  Don't exit when any command fails (grep)
-#     set +e
-
-#     ##  Find all code addresses 23?????? in the Output Log, remove duplicates, skip 23007000.
-#     ##  Returns a newline-delimited list of addresses: "23011000\n230053a0\n..."
-#     grep --extended-regexp \
-#         --only-matching \
-#         "23[0-9a-f]{6}" \
-#         /tmp/test.log \
-#         | grep -v "23007000" \
-#         | uniq \
-#         >/tmp/test.addr
-
-#     ##  For every address, show the corresponding line in the disassembly
-#     for addr in $(cat /tmp/test.addr); do
-#         ##  Skip addresses that don't match
-#         match=$(grep "$addr:" /tmp/nuttx.S)
-#         if [ "$match" != "" ]; then
-#             echo "----- Code Address $addr"
-#             grep \
-#                 --context=5 \
-#                 --color=auto \
-#                 "$addr:" \
-#                 /tmp/nuttx.S
-#             echo
-#         fi
-#     done
-
-#     ##  Find all data addresses 42?????? in the Output Log, remove duplicates.
-#     ##  Returns a newline-delimited list of addresses: "23011000\n230053a0\n..."
-#     grep --extended-regexp \
-#         --only-matching \
-#         "42[0-9a-f]{6}" \
-#         /tmp/test.log \
-#         | uniq \
-#         >/tmp/test.addr
-
-#     ##  For every address, show the corresponding line in the disassembly
-#     for addr in $(cat /tmp/test.addr); do
-#         ##  Skip addresses that don't match
-#         match=$(grep "^$addr" /tmp/nuttx.S)
-#         if [ "$match" != "" ]; then
-#             echo "----- Data Address $addr"
-#             grep \
-#                 --color=auto \
-#                 "^$addr" \
-#                 /tmp/nuttx.S \
-#                 | grep -v "noinit"
-#             echo
-#         fi
-#     done
-
-#     ##  Exit when any command fails
-#     set -e
-# fi
